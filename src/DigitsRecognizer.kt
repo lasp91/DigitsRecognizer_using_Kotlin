57891a4dd303d14a4e0b5c809aec0e39896ac1e2
Luiz Peyon <la.de.peyon@xtra.co.nz>
2017-01-01 08:52:18 +1300

    Added manhattanDistance2 - uses imperative code Tried to use a alias for Distance, but this have to wait until Kotlin 1.2 is released Using map() and average() in the evaluate lambda

--- src/DigitsRecognizer.kt
+++ src/DigitsRecognizer.kt
@@ -28,19 +28,33 @@ fun main(args: Array<String>)
   val validationPath = "./Data/validationsample.csv"
   val validationData = reader(validationPath)
 
-  val manhattanDistance : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->
+  val manhattanDistance : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->   // Using zip and map
     val sum = pixels1.zip(pixels2)
         .map { p -> Math.abs(p.first - p.second) }
         .sum()
     sum
   }
 
+  val manhattanDistance2 : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->  // Using all imperative
+    var dist = 0
+
+    for (i in 0 .. pixels1.size - 1)
+    {
+      dist += Math.abs(pixels1[i] - pixels2[i])
+    }
+
+    dist
+  }
+
   val euclideanDistance : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->
       val sum = pixels1.zip(pixels2)
-        .map { p -> Math.pow(Math.abs(p.first - p.second).toDouble(), 2.0) }
+//          .map { p -> Math.pow((p.first - p.second).toDouble(), 2.0) }
+          .map({ p -> val dist = (p.first - p.second); dist * dist })
         .sum()
-    sum.toInt()
+    sum
   }
+
+  val Distance : (IntArray, IntArray) -> Int  // There should be a way of using this as a type....
 
   val classify : (Array<Observation>, (IntArray, IntArray) -> Int, IntArray) -> String = { trainingSet, dist, pixels ->
     val observation =  trainingSet.minBy { x -> dist(x.Pixels, pixels) }
@@ -48,16 +62,28 @@ fun main(args: Array<String>)
   }
 
   val manhattanClassifier : (IntArray) -> String = { pixels ->
-    classify(trainingData, manhattanDistance, pixels)
+    classify(trainingData, manhattanDistance2, pixels)
   }
 
   val evaluate : (Array<Observation>, (IntArray) -> String) -> Unit = { validationSet, classifier ->
-    val count = validationSet.size
     val average = validationSet
-        .sumBy { x -> if (classifier(x.Pixels) == x.label) { 1 } else { 0 } }
-        .toDouble() / count
+        .map { x -> if (classifier(x.Pixels) == x.label) { 1 } else { 0 } }
+        .average()
     println("Correct: $average")
   }
+//    val count = validationSet.size
+//    var sum = 0
+//
+//    for (i in 0 .. count - 1)
+//    {
+//      if (classifier(validationSet[i].Pixels) == validationSet[i].label)
+//      {
+//        sum += 1
+//      }
+//    }
+//
+//    println("Correct:  ${sum.toDouble() / count}")
+//  }
 
   val startTime = System.currentTimeMillis()
 
