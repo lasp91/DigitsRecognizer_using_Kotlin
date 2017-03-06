0dd42b116553141883e78dd4b2836d6b3a618804
Luiz Peyon <la.de.peyon@xtra.co.nz>
2017-03-07 11:57:24 +1300

    Now using the Observation data class out of the main function, because started to use "typealias", which can't be local. Converted some lambdas to functions, i.e. now using lambdas just when it seems more needed. More tidy ups with source code line breaks and for other things.

--- src/DigitsRecognizer.kt
+++ src/DigitsRecognizer.kt
@@ -1,25 +1,28 @@
 import java.io.File
 
+data class Observation (val label: String , val Pixels: IntArray)
+
+typealias Distance = (IntArray, IntArray) -> Int
+typealias Classifier = (IntArray) -> String
+typealias Observations = Array<Observation>
+
 fun main(args: Array<String>)
 {
-  data class Observation (val label: String , val Pixels: IntArray)
-
   fun observationData(csvData: String) : Observation
   {
     val columns = csvData.split(',')
     val label = columns[0]
-    val pixels = columns.subList(1, columns.lastIndex)
-        .map { it -> it.toInt() }
+    val pixels = columns.subList(1, columns.lastIndex).map(String::toInt)
+
     return Observation(label, pixels.toIntArray())
   }
 
   fun reader(path: String) : Array<Observation>
   {
-    val lines = File(path)
-        .readLines()
-        .drop(1)
-        .map { it -> observationData(it) }
-    return lines.toTypedArray()
+    val observations = File(path).useLines { lines ->
+        lines.drop(1).map(::observationData).toList().toTypedArray() }
+
+    return observations
   }
 
   val trainingPath = "./Data/trainingsample.csv"
@@ -28,46 +31,48 @@ fun main(args: Array<String>)
   val validationPath = "./Data/validationsample.csv"
   val validationData = reader(validationPath)
 
-  val manhattanDistance : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->   // Using zip and map
-    val sum = pixels1.zip(pixels2)
+  val manhattanDistance = { pixels1: IntArray, pixels2: IntArray ->   // Using zip and map
+    val dist = pixels1.zip(pixels2)
         .map { p -> Math.abs(p.first - p.second) }
         .sum()
-    sum
+    dist
   }
 
-  val manhattanDistance2 : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->  // Using all imperative
+  val manhattanDistance2 = fun(pixels1: IntArray, pixels2: IntArray) : Int // Using all imperative
+  {
     var dist = 0
 
-    for (i in 0 .. pixels1.size - 1)
+    for (i in 0 until pixels1.size)
     {
       dist += Math.abs(pixels1[i] - pixels2[i])
     }
 
+    return dist
+  }
+
+  val euclideanDistance = { pixels1: IntArray, pixels2: IntArray ->
+      val dist = pixels1.zip(pixels2)
+//        .map { p -> Math.pow((p.first - p.second).toDouble(), 2.0) }
+        .map({ p -> val dist = (p.first - p.second); dist * dist })
+        .sum()
     dist
   }
 
-  val euclideanDistance : (IntArray, IntArray) -> Int = { pixels1, pixels2 ->
-      val sum = pixels1.zip(pixels2)
-//          .map { p -> Math.pow((p.first - p.second).toDouble(), 2.0) }
-          .map({ p -> val dist = (p.first - p.second); dist * dist })
-        .sum()
-    sum
-  }
-
-  val Distance : (IntArray, IntArray) -> Int  // There should be a way of using this as a type....
-
-  val classify : (Array<Observation>, (IntArray, IntArray) -> Int, IntArray) -> String = { trainingSet, dist, pixels ->
+  fun classify(trainingSet: Observations, dist: Distance, pixels: IntArray) : String
+  {
     val observation =  trainingSet.minBy { x -> dist(x.Pixels, pixels) }
-    observation!!.label
+    return observation!!.label
   }
 
-  val manhattanClassifier : (IntArray) -> String = { pixels ->
-    classify(trainingData, manhattanDistance2, pixels)
+  val manhattanClassifier = fun(pixels: IntArray) : String
+  {
+    return classify(trainingData, manhattanDistance2, pixels)
   }
 
-  val evaluate : (Array<Observation>, (IntArray) -> String) -> Unit = { validationSet, classifier ->
+  fun evaluate(validationSet: Observations, classifier: Classifier) : Unit
+  {
     val average = validationSet
-        .map { x -> if (classifier(x.Pixels) == x.label) { 1 } else { 0 } }
+        .map { x -> if (classifier(x.Pixels) == x.label) 1.0 else 0.0 }
         .average()
     println("Correct: $average")
   }
