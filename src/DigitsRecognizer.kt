4dab5842fb2e1ae4a8ba5244650b28a6c9702fc6
Luiz Peyon <la.de.peyon@xtra.co.nz>
2017-03-26 12:46:41 +1300

    Moved functions definitions out of the "main" function. Now using Kotlin 1.1 which allows type alias. Added "manhattanDistanceParallel" for testing purposes. Some tidy-ups.

--- src/DigitsRecognizer.kt
+++ src/DigitsRecognizer.kt
@@ -1,4 +1,7 @@
 import java.io.File
+import java.util.stream.IntStream
+
+import java.util.concurrent.atomic.AtomicInteger
 
 data class Observation (val label: String , val Pixels: IntArray)
 
@@ -6,76 +9,71 @@ typealias Distance = (IntArray, IntArray) -> Int
 typealias Classifier = (IntArray) -> String
 typealias Observations = Array<Observation>
 
-fun main(args: Array<String>)
+fun observationData(csvData: String) : Observation
 {
-  fun observationData(csvData: String) : Observation
-  {
-    val columns = csvData.split(',')
-    val label = columns[0]
-    val pixels = columns.subList(1, columns.lastIndex).map(String::toInt)
+  val columns = csvData.split(',')
+  val label = columns[0]
+  val pixels = columns.subList(1, columns.lastIndex).map(String::toInt)
 
-    return Observation(label, pixels.toIntArray())
-  }
+  return Observation(label, pixels.toIntArray())
+}
 
-  fun reader(path: String) : Array<Observation>
-  {
-    val observations = File(path).useLines { lines ->
-        lines.drop(1).map(::observationData).toList().toTypedArray() }
+fun reader(path: String) : Array<Observation>
+{
+  val observations = File(path).useLines { lines ->
+      lines.drop(1).map(::observationData).toList().toTypedArray() }
 
-    return observations
-  }
+  return observations
+}
 
-  val trainingPath = "./Data/trainingsample.csv"
-  val trainingData = reader(trainingPath)
+val manhattanDistance = { pixels1: IntArray, pixels2: IntArray ->   // Using zip and map
+  val dist = pixels1.zip(pixels2)
+      .map { p -> Math.abs(p.first - p.second) }
+      .sum()
+  dist
+}
 
-  val validationPath = "./Data/validationsample.csv"
-  val validationData = reader(validationPath)
+val manhattanDistanceImperative = fun(pixels1: IntArray, pixels2: IntArray) : Int
+{
+  var dist = 0
 
-  val manhattanDistance = { pixels1: IntArray, pixels2: IntArray ->   // Using zip and map
+  for (i in 0 until pixels1.size)
+    dist += Math.abs(pixels1[i] - pixels2[i])
+
+  return dist
+}
+
+val manhattanDistanceParallel = fun(pixels1: IntArray, pixels2: IntArray) : Int
+{
+  val dist = AtomicInteger(0)
+
+  IntStream.range(0, pixels1.size).parallel().forEach { i ->
+    dist.addAndGet(Math.abs(pixels1[i] - pixels2[i])) }
+
+  return dist.toInt()
+}
+
+val euclideanDistance = { pixels1: IntArray, pixels2: IntArray ->
     val dist = pixels1.zip(pixels2)
-        .map { p -> Math.abs(p.first - p.second) }
-        .sum()
-    dist
-  }
+//      .map { p -> Math.pow((p.first - p.second).toDouble(), 2.0) }
+      .map({ p -> val dist = (p.first - p.second); dist * dist })
+      .sum()
+  dist
+}
 
-  val manhattanDistance2 = fun(pixels1: IntArray, pixels2: IntArray) : Int // Using all imperative
-  {
-    var dist = 0
+fun classify(trainingSet: Array<Observation>, dist: Distance, pixels: IntArray) : String
+{
+  val observation =  trainingSet.minBy { (_, Pixels) -> dist(Pixels, pixels) }
+  return observation!!.label
+}
 
-    for (i in 0 until pixels1.size)
-    {
-      dist += Math.abs(pixels1[i] - pixels2[i])
-    }
-
-    return dist
-  }
-
-  val euclideanDistance = { pixels1: IntArray, pixels2: IntArray ->
-      val dist = pixels1.zip(pixels2)
-//        .map { p -> Math.pow((p.first - p.second).toDouble(), 2.0) }
-        .map({ p -> val dist = (p.first - p.second); dist * dist })
-        .sum()
-    dist
-  }
-
-  fun classify(trainingSet: Observations, dist: Distance, pixels: IntArray) : String
-  {
-    val observation =  trainingSet.minBy { x -> dist(x.Pixels, pixels) }
-    return observation!!.label
-  }
-
-  val manhattanClassifier = fun(pixels: IntArray) : String
-  {
-    return classify(trainingData, manhattanDistance2, pixels)
-  }
-
-  fun evaluate(validationSet: Observations, classifier: Classifier) : Unit
-  {
-    val average = validationSet
-        .map { x -> if (classifier(x.Pixels) == x.label) 1.0 else 0.0 }
-        .average()
-    println("Correct: $average")
-  }
+fun evaluate(validationSet: Array<Observation>, classifier: (IntArray) -> String) : Unit
+{
+  val average = validationSet
+      .map({ (label, Pixels) -> if (classifier(Pixels) == label) 1.0 else 0.0 })
+      .average()
+  println("Correct: $average")
+}
 //    val count = validationSet.size
 //    var sum = 0
 //
@@ -89,6 +87,21 @@ fun main(args: Array<String>)
 //
 //    println("Correct:  ${sum.toDouble() / count}")
 //  }
+
+//-----------------------------------------------------------------------------------------
+
+fun main(args: Array<String>)
+{
+  val trainingPath = "./Data/trainingsample.csv"
+  val trainingData = reader(trainingPath)
+
+  val validationPath = "./Data/validationsample.csv"
+  val validationData = reader(validationPath)
+
+  val manhattanClassifier = fun(pixels: IntArray) : String
+  {
+    return classify(trainingData, manhattanDistanceImperative, pixels)
+  }
 
   val startTime = System.currentTimeMillis()
 
---
+++ src/Form.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ladep_000 on 2/19/2017.
+ */
+public class Form
+{
+}
